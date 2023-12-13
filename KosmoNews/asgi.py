aec116dd56c7a1788e59aa2535925fb70732a189
donko1 <zalbertic@mail.ru>
2023-12-13 18:31:21 +0300

    first commit

---
+++ KosmoNews/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for KosmoNews project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'KosmoNews.settings')
+
+application = get_asgi_application()
