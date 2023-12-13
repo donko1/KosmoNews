691dfb979aa15dc365654f764a66235c0da5ad4a
donko1 <zalbertic@mail.ru>
2023-12-13 19:04:21 +0300

    add model. Make first view(nothing to return)

--- KosmoNews/settings.py
+++ KosmoNews/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-linz-_4oj4bln9+#2s!wf^oax4ttin1t&2+rfm@*s%nct-q19^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "news"
 ]
 
 MIDDLEWARE = [
--- news/models.py
+++ news/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class News(models.Model):
+    THEMES = (
+        ('theme1', 'Политика'),
+        ("theme2", "Регионы")
+    )
+
+    date = models.DateTimeField(default=timezone.now)
+    title = models.CharField(max_length=200)
+    article_text = models.TextField()
+    theme = models.CharField(max_length=50, choices=THEMES, default='')
+
+    def __str__(self):
+        return self.title
