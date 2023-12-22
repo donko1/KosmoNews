8a5a3e61aae90cbda0a3696d683b345805bee509
donko1 <zalbertic@mail.ru>
2023-12-22 19:44:20 +0300

    verison 1.0. U can use without react server

--- KosmoNews/urls.py
+++ KosmoNews/urls.py
@@ -15,11 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path("", TemplateView.as_view(template_name='index.html')),
     path('admin/', admin.site.urls),
     path("api/", include("news.urls")),
+    path(r'', TemplateView.as_view(template_name="index.html")),
+    re_path(r'^(?P<number>\d+)/$', TemplateView.as_view(template_name="index.html")),
+    re_path(r'^(?P<word>\D+)(?P<number>\d+)/$', TemplateView.as_view(template_name="index.html")),
 ]
